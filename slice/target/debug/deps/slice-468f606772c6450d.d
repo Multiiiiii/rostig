ccb55c77668024957162f6533ecc948754117601
Multiiiiii <multimitamiensaft@gmail.com>
2024-05-24 11:19:12 +0200

    aeiue

---
+++ slice/target/debug/deps/slice-468f606772c6450d.d
@@ -0,0 +1,5 @@
+/home/m/Documents/rostig/slice/target/debug/deps/libslice-468f606772c6450d.rmeta: src/main.rs
+
+/home/m/Documents/rostig/slice/target/debug/deps/slice-468f606772c6450d.d: src/main.rs
+
+src/main.rs:
---
+++ slice/target/debug/deps/slice-7bee58dd4cde084e.d
@@ -0,0 +1,5 @@
+/home/m/Documents/rostig/slice/target/debug/deps/libslice-7bee58dd4cde084e.rmeta: src/main.rs
+
+/home/m/Documents/rostig/slice/target/debug/deps/slice-7bee58dd4cde084e.d: src/main.rs
+
+src/main.rs:
