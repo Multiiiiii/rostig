3a4c10a9537c2607475d925d7112de1789950940
Multiiiiii <multimitamiensaft@gmail.com>
2024-05-25 15:36:59 +0200

    slices

---
+++ enums/src/main.rs
@@ -0,0 +1,87 @@
+enum IpAddrKand {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: Sting,
+}
+
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
+enum IpAddr2 {
+    V4(u8,u8,u8),
+    V6(String),
+}
+
+struct Ipv6Addr{
+    // --snip--
+}
+
+struct Ipv4Addr{
+    // --snip--
+}
+enum IpAddrStr {
+    V4(Ipv4Addr),
+    V6(Ipv6Addr),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        //mothod body would be defined here
+    }
+}
+
+struct QuitMessage; //unit struct
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+struct WriteMessage(String); //tubple struct
+struct ChangeColorMessage(i32,i32, i32); //tuple struct
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1")
+    }
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        addess: String::from("::1"),
+    }
+
+    let home =IpAddr::V1(String::from("127.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let home = IpAddr2::V4(127,0,0,1);
+    let loopback = IpAddr2::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    let absent_number :Option<i32> = None;
+
+}
+
+fn route(ip_kind: IpAddrKind){}
