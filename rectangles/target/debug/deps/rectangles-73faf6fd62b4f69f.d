3cb8810cabc460fe9f931d8b38a73ec2685cb676
Multiiiiii <multimitamiensaft@gmail.com>
2024-05-25 15:36:41 +0200

    rectangles finished

---
+++ rectangles/target/debug/deps/rectangles-73faf6fd62b4f69f.d
@@ -0,0 +1,5 @@
+/home/m/Documents/rostig/rectangles/target/debug/deps/librectangles-73faf6fd62b4f69f.rmeta: src/main.rs
+
+/home/m/Documents/rostig/rectangles/target/debug/deps/rectangles-73faf6fd62b4f69f.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/deps/rectangles-ea3535c7fd74334d.d
@@ -0,0 +1,5 @@
+/home/m/Documents/rostig/rectangles/target/debug/deps/librectangles-ea3535c7fd74334d.rmeta: src/main.rs
+
+/home/m/Documents/rostig/rectangles/target/debug/deps/rectangles-ea3535c7fd74334d.d: src/main.rs
+
+src/main.rs:
