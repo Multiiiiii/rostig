4ab155cacb588c627602496c5ef1183a1328003c
Multiiiiii <multimitamiensaft@gmail.com>
2024-05-28 09:26:11 +0200

    dk

---
+++ matches/target/debug/deps/matches-266e3de639a36289.d
@@ -0,0 +1,5 @@
+/home/m/Documents/rostig/matches/target/debug/deps/libmatches-266e3de639a36289.rmeta: src/main.rs
+
+/home/m/Documents/rostig/matches/target/debug/deps/matches-266e3de639a36289.d: src/main.rs
+
+src/main.rs:
---
+++ matches/target/debug/deps/matches-a15270cb94a3f081.d
@@ -0,0 +1,5 @@
+/home/m/Documents/rostig/matches/target/debug/deps/libmatches-a15270cb94a3f081.rmeta: src/main.rs
+
+/home/m/Documents/rostig/matches/target/debug/deps/matches-a15270cb94a3f081.d: src/main.rs
+
+src/main.rs:
