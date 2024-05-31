56071f5d98545658348b387c02b92be88e720bba
Multiiiiii <multimitamiensaft@gmail.com>
2024-05-31 14:24:32 +0200

    consicseness

---
+++ concise/target/debug/deps/concise-5b4d72d54b73615d.d
@@ -0,0 +1,5 @@
+/home/m/Documents/rostig/concise/target/debug/deps/libconcise-5b4d72d54b73615d.rmeta: src/main.rs
+
+/home/m/Documents/rostig/concise/target/debug/deps/concise-5b4d72d54b73615d.d: src/main.rs
+
+src/main.rs:
---
+++ concise/target/debug/deps/concise-ccd5f496e5deade6.d
@@ -0,0 +1,5 @@
+/home/m/Documents/rostig/concise/target/debug/deps/libconcise-ccd5f496e5deade6.rmeta: src/main.rs
+
+/home/m/Documents/rostig/concise/target/debug/deps/concise-ccd5f496e5deade6.d: src/main.rs
+
+src/main.rs:
